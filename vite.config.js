447374cd97807340d3d845b498793d0d2bdb78d7
Olha Paliienko <olha_suchok@ukr.net>
2025-12-06 15:44:30 +0200

    Added tailwind, deleted unused files, added css global files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { readdirSync } from "fs";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import tailwindcss from "@tailwindcss/vite";
+
+const landingsDirectory = resolve(__dirname, "src/landings");
+
+const landings = readdirSync(landingsDirectory, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
+const landingInputs = landings.reduce((acc, name) => {
+  acc[name] = resolve(__dirname, `src/landings/${name}/index.html`);
+  return acc;
+}, {});
+
+export default defineConfig(({ command }) => {
+  const isProduction = command === "build";
+
+  return {
+    plugins: [react(), tailwindcss()],
+    css: {
+      modules: {
+        localsConvention: "camelCase",
+        generateScopedName: "[name]__[local]___[hash:base64:5]",
+      },
+    },
+    build: {
+      rollupOptions: {
+        input: isProduction
+          ? landingInputs
+          : {
+              main: resolve(__dirname, "index.html"),
+              ...landingInputs,
+            },
+      },
+    },
+  };
+});
