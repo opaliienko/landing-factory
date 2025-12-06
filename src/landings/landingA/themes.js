0b75746d9d65ffb0a9b454dee100cd14c9f68c6b
Olha Paliienko <olha_suchok@ukr.net>
2025-12-06 19:27:01 +0200

    Added landing themes

---
+++ src/landings/landingA/themes.js
@@ -0,0 +1,28 @@
+export const THEMES = {
+  colors: {
+    TEXT_COLOR: "#030712",
+
+    MAIN_BG_COLOR: "#D5F0D9",
+    SECONDARY_BG_COLOR: "#f9fafb",
+
+    ACCENT_COLOR: "#296248",
+
+    BUTTON_BG_COLOR: "#e5e7eb",
+
+    white: {
+      100: "#FFFFFF",
+    },
+
+    black: {
+      100: "#000000",
+    },
+  },
+
+  fonts: {
+    fontsFamily: {
+      medium: "Nunito-Medium",
+      bold: "Nunito-Bold",
+      extraBold: "Nunito-ExtraBold",
+    },
+  },
+};
---
+++ src/landings/landingB/themes.js
@@ -0,0 +1,28 @@
+export const THEMES = {
+  colors: {
+    TEXT_COLOR: "#030712",
+
+    MAIN_BG_COLOR: "#BACBD9",
+    SECONDARY_BG_COLOR: "#F2F2F2",
+
+    ACCENT_COLOR: "#1D1340",
+
+    BUTTON_BG_COLOR: "#e5e7eb",
+
+    white: {
+      100: "#FFFFFF",
+    },
+
+    black: {
+      100: "#000000",
+    },
+  },
+
+  fonts: {
+    fontsFamily: {
+      medium: "Nunito-Medium",
+      bold: "Nunito-Bold",
+      extraBold: "Nunito-ExtraBold",
+    },
+  },
+};
