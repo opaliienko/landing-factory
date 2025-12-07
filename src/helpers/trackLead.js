14fa492abb495257ce6e77cc07b6bd468d056fa4
Olha Paliienko <olha_suchok@ukr.net>
2025-12-07 18:08:49 +0200

    Simulated the Lead event

--- src/helpers/trackLead.js
+++ src/helpers/trackLead.js
@@ -1,6 +1,10 @@
 import { toast } from "react-toastify";
 
 export const trackLead = (message, landingId) => {
+  // if (window.fbq) {
+  //   window.fbq("track", "Lead");
+  // }
+
   console.log(`Подія Lead відправлена! Сайт: ${landingId}.`);
   toast.success(message);
 };
