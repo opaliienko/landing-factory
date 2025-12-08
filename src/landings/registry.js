ef4f4a22fc42bfdcb117a14d3a32bc169221edbb
Olha Paliienko <olha_suchok@ukr.net>
2025-12-08 15:42:41 +0200

    Removed dev folders

--- src/landings/registry.js
+++
@@ -1,7 +0,0 @@
-import { config as landing1Config } from "./landing-1/config";
-import { config as landing2Config } from "./landing-2/config";
-
-export const landingsRegistry = {
-  landing1: landing1Config,
-  landing2: landing2Config,
-};
