dc481dda2fdb3a5d3588d06cf6dc2663dc3a0333
Olha Paliienko <olha_suchok@ukr.net>
2025-12-08 15:48:17 +0200

    Separated css files

--- src/landings/landing-1/main.jsx
+++ src/landings/landing-1/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import PageBuilder from "../../builders/PageBuilder";
 import { config } from "./config";
 
-import "../../styles/global.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PageBuilder config={config} />
