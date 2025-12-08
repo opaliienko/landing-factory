895349aaa6efb8cf5972dc47381c47b28b117e87
Olha Paliienko <olha_suchok@ukr.net>
2025-12-08 22:47:32 +0200

    Fixed

--- src/builders/PageBuilder.jsx
+++ src/builders/PageBuilder.jsx
@@ -2,7 +2,7 @@ import { ToastContainer } from "react-toastify";
 
 import Hero from "../components/sections/Hero";
 import Features from "../components/sections/Features";
-import Reviews from "../components/sections/reviews";
+import Reviews from "../components/sections/Reviews";
 import Footer from "../components/sections/Footer";
 
 const sectionMap = { Hero, Features, Reviews, Footer };
