7357da7a9d2f92f68d69b546a02f2815a1a85c9d
Olha Paliienko <olha_suchok@ukr.net>
2025-12-08 22:29:52 +0200

    Fixed

--- src/landings/landing-1/config.js
+++ src/landings/landing-1/config.js
@@ -49,7 +49,7 @@ export const config = {
       type: "CustomSection",
       data: {
         title: "My custom section",
-        text: "My custom text",
+        text: "(for example)",
       },
     },
     {
