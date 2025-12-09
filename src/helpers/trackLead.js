2915db8e05d27c0281cbcc3de96697cbbfcbe7bb
Olha Paliienko <olha_suchok@ukr.net>
2025-12-09 10:45:09 +0200

    Fixed trackLead

--- src/helpers/trackLead.js
+++ src/helpers/trackLead.js
@@ -2,13 +2,14 @@ import { toast } from "react-toastify";
 
 export const trackLead = (message, landingId, background) => {
   if (window.fbq) {
-    window.fbq("track", "Lead");
-  } else {
+    window.fbq();
     console.log(`Lead (умовний) для ${landingId} відправлено.`);
     toast.success(message, {
       style: {
         background: background,
       },
     });
+  } else {
+    console.warn("FB Pixel не завантажено.");
   }
 };
